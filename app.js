97ce2cca2692252abf9900f393a191fa20d51f81
Salam747 <159710405+Salam747@users.noreply.github.com>
2024-05-22 16:43:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+
 // Qustion No:01
 console.log("Assala o Alaikum my name is Abdul Salam");
 //__________________________________________________________________________________________________
